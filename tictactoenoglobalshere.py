8a0f9c52a96c92416658ad286c7a28d8bf94cd7c
Myrelin <anna.iklody@gmail.com>
2018-05-31 22:02:15 +0200

    fix: board clear, welcome message added

--- tictactoenoglobalshere.py
+++ tictactoenoglobalshere.py
@@ -1,5 +1,21 @@
 import os
 
+def welcome():
+    print("\n\n\n\n\n\n\n\n\n\n")
+    print('\033[96m\033[1m* * * * * * * * * * * * * * * * * * * * \033[0m') 
+    print()
+    print('\033[91m\033[1mW e l c o m e  t o  T i c - T a c - T o e !\033[0m')
+    print()
+    print('\033[96m\033[1m* * * * * * * * * * * * * * * * * * * * \033[0m')
+    print()
+    print('\033[94mCode by: Dora Koreny, Anna Iklody\033[0m')
+    print()
+    print('\033[96m\033[1m* * * * * * * * * * * * * * * * * * * *\033[0m')
+    print()
+    print('\033[91m\033[1mS h a l l   w e   p l a y   a   g a m e ?\033[0m')
+    print()
+    print("\n\n\n\n\n")
+
 def createboard():
     board = [0,1,2,3,4,5,6,7,8,9]
     return board
@@ -23,7 +39,6 @@ def player2_start():
     return p2
 
 def player1(p1, board):
-    
     try:      
         p1_input = int(input(p1 + '\033[94m, enter a number to place your x (1-9):\033[0m'))
         if board[p1_input] != '\033[94mx\033[0m' and board[p1_input] != '\033[91mo\033[0m' and board[p1_input] != 0:
@@ -31,8 +46,6 @@ def player1(p1, board):
             clear()
             showboard(board) 
             tiecheck(board)
-             
-           
         else:
             print("\nChoose another place!")
             player1(p1, board)
@@ -44,7 +57,6 @@ def player1(p1, board):
         player1(p1, board)
     
 def player2(p2, board):
-    
     try:
         p2_input = int(input(p2 + '\033[91m, enter a number to place your o (1-9):\033[0m'))
         if board[p2_input] != '\033[94mx\033[0m' and board[p2_input] != '\033[91mo\033[0m' and board[p2_input] != 0:
@@ -52,7 +64,6 @@ def player2(p2, board):
             clear()
             showboard(board)
             tiecheck(board)
-            
         else:
             print("\nChoose another place!")  
             player2(p2, board)   
@@ -87,31 +98,22 @@ def check_win(board):
             elif board[7] == board[8] == board[9]:
                 return "Win"
             elif board[1] == board[4] == board[7]:
-               
                 return "Win"
             elif board[2] == board[5] == board[8]:
-               
                 return "Win"
             elif board[3] == board[6] == board[9]:
-               
                 return "Win"
             elif board[1] == board[5] == board[9]:
-               
                 return "Win"
             elif board[3] == board[5] == board[7]:
-               
                 return "Win"
-
-
-
-
-
 
 def keep_playing(continue_game):
         continue_game = input("Do you want to keep playing? (yes/no)")
         if continue_game == "yes":
             clear()
-            main()   
+            main()
+              
         elif continue_game == "no":
             clear()
             print('\n\n\n\n\n\n\n\n\n\n\033[1mThank you for playing!\033[0m')
@@ -119,39 +121,38 @@ def keep_playing(continue_game):
             keep_playing(continue_game)
         
 def main():
-    board = createboard()
-    showboard(board)
-   
+    clear()
+    welcome()
+    board = createboard()   
     p1 = player1_start()
     p2 = player2_start()
     clear()
+    showboard(board)
     continue_game = "y"
 
     while continue_game == "y":
+        
         player1(p1, board)
         if check_win(board) == "Win":
-            print("please fucking work")
+            print(p1,'\033[92m, congratulations you won!\033[0m')
+            board = createboard()
             keep_playing(continue_game)
             break
-
         elif tiecheck(board) == "Tie":
-            print("tie the thing already")
+            print("It's a tie!")
+            board = createboard()
             keep_playing(continue_game)
             break
  
         player2(p2, board)
         if check_win(board) == "Win":
-            print("work here too kthx")
+            print(p2,'\033[92m, congratulations you won!\033[0m')
+            board = createboard()
             keep_playing(continue_game)
             break
         elif tiecheck(board) == "Tie":
-            print("tie the thing already")
+            print("It's a tie!")
+            board = createboard()
             keep_playing(continue_game)
             break
-    
-
-        
-      
-      
-
 main()
